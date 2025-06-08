b41e2031e040a52aee1370a65f78ff05ab7c93c6
Le Nguyen To <lenguyento61@gmail.com>
2025-06-08 15:42:06 +0700

    C Repository

---
+++ Server/src/main/java/org/example/server/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package org.example.server.repository;
+
+import org.example.server.entity.GroupEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
+}
